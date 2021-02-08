412c893e8c02a27f17901f356ca6338bfe3679f0
Deepak Swami <brahminboy123456@gmail.com>
2021-02-08 10:43:31 +0530

    This is My First Commit for this project

---
+++ shoppingbackendPractical/src/main/java/net/swami/shoppingbackendPractical/App.java
@@ -0,0 +1,13 @@
+package net.swami.shoppingbackendPractical;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
